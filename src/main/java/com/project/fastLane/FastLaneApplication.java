1612208b5ad3ee69b2bda45ee98a454b7a078887
임또이 <sh663746@gmail.com>
2021-09-27 18:41:00 +0900

    init

---
+++ src/main/java/com/project/fastLane/FastLaneApplication.java
@@ -0,0 +1,13 @@
+package com.project.fastLane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastLaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastLaneApplication.class, args);
+	}
+
+}
